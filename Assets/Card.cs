498e11eb6c41a2f430aa984d76620b5691dcd2ec
MarcoFormatica <marcoformatica@gmail.com>
2024-12-04 17:45:25 +0100

    1

---
+++ Assets/Card.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ESeed
+{
+    Bastoni,
+    Spade,
+    Coppe,
+    Denari
+}
+public class Card : MonoBehaviour
+{
+
+
+    public int GetPoints()
+    {
+
+        return 0;
+
+    }
+
+    public int GetPower()
+    {
+
+        return 0;
+    }
+}
